332e29942dc41720e848b896c666838fbe4c16af
Beirut Bites <beirut.bites.official@gmail.com>
2026-09-17 22:53:42 +0300

    Add next 20 verified catalog product images

---
+++ database/product-images-batch-05.js
@@ -0,0 +1,107 @@
+// Supplemental curated product-image mappings.
+// These entries extend database/product-images.js without rewriting the large base mapping.
+
+const EXTRA_PRODUCT_IMAGES = [
+  {
+    brand: 'TUBBEES',
+    name: 'JAR MATCHA MADE IN HEAVEN',
+    image: 'https://us.tubbees.com/cdn/shop/files/MatchaMadeinHeaven.png?v=1762687117&width=1946',
+  },
+  {
+    brand: 'TUBBEES',
+    name: 'JAR BLUEBERRY SORBET',
+    image: 'https://us.tubbees.com/cdn/shop/files/BlueBerrySorbet.png?v=1762687113&width=1946',
+  },
+  {
+    brand: 'TUBBEES',
+    name: 'JAR TIRA-MISS-YOU',
+    image: 'https://us.tubbees.com/cdn/shop/files/TiraMissYou_772efa81-23b3-449d-845f-bfb3ee7e9a71.png?v=1762687110&width=1946',
+  },
+  {
+    brand: 'TUBBEES',
+    name: 'JAR TRES LECHES',
+    image: 'https://us.tubbees.com/cdn/shop/files/TresLeches_a04885b7-3409-4fe5-9d5f-b65ffd30af4f.png?v=1762687112&width=1946',
+  },
+  {
+    brand: 'TUBBEES',
+    name: 'JAR PASSION FRUIT MOJITO',
+    image: 'https://us.tubbees.com/cdn/shop/files/PassionFruitMojito.png?v=1762687118&width=1946',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'MULTIPLEX 3D LIP GLOSS  N:05',
+    image: 'https://farmake.com.br/cdn/shop/files/6be7e48f351ed1a41e03c588e442749e.png?v=1737812445&width=1445',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'MULTIPLEX 3D LIP GLOSS  N:07',
+    image: 'https://farmake.com.br/cdn/shop/files/6be7e48f351ed1a41e03c588e442749e.png?v=1737812445&width=1445',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'MULTIPLEX 3D LIP GLOSS N:12',
+    image: 'https://farmake.com.br/cdn/shop/files/6be7e48f351ed1a41e03c588e442749e.png?v=1737812445&width=1445',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'MULTIPLEX 3D LIP GLOSS N:01',
+    image: 'https://farmake.com.br/cdn/shop/files/6be7e48f351ed1a41e03c588e442749e.png?v=1737812445&width=1445',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'MULTIPLEX 3D LIP GLOSS N:11',
+    image: 'https://farmake.com.br/cdn/shop/files/6be7e48f351ed1a41e03c588e442749e.png?v=1737812445&width=1445',
+  },
+  {
+    brand: 'AUCEACADEMY',
+    name: 'ARTIST CONCEALER',
+    image: 'https://blondas.com.py/cdn/shop/files/alice-academy-paleta-correctivo-blondas-cosmeticos-blondas-8460518.jpg?v=1780076168&width=1024',
+  },
+  {
+    brand: 'PARTY QUEEN',
+    name: 'TREATMENT CLEANING MILK',
+    image: 'https://dittuti.com/wp-content/uploads/2022/04/cleansing-milk-1.png',
+  },
+  {
+    brand: 'PARTY QUEEN',
+    name: 'WATER-PROOF LONGLASTING KOHL EYE PENCIL',
+    image: 'https://ae01.alicdn.com/kf/HTB1uVkOIpXXXXXVXXXXq6xXFXXXE/Party-Queen-Long-Lasting-Waterproof-Kohl-Eye-Pencil-Matte-Black-Ultra-Smudge-proof-Gel-Eyeliner-Makeup.jpg',
+  },
+  {
+    brand: 'PARTY QUEEN',
+    name: 'EYE LAP PANCIL WATER PROOF',
+    image: 'https://tiimg.tistatic.com/fp/1/005/453/party-queen-eye-enhancing-black-kajal-eyeliner-stick-solid-thick-eyeliner-gel-makeup-smooth-waterproof-natural-smoky-eyes-liner--816.jpg',
+  },
+  {
+    brand: 'DAKKA KADIMA',
+    name: 'SOAP GOLDEN',
+    image: 'https://i.ebayimg.com/images/g/TbQAAOSwQRRoWiip/s-l1200.jpg',
+  },
+  {
+    brand: 'DAKKA KADIMA',
+    name: 'SOAP MUSK AL ROMMAN',
+    image: 'https://www.authentikue.com/cdn/shop/files/Pomegranate_Musk_Aleppo_Laurel_Soap_by_Dakka_Kadima.heic?v=1758710793&width=1946',
+  },
+  {
+    brand: 'MAKEOVER',
+    name: 'MIST & FIX MAKE-UP FIXER MIST',
+    image: 'https://i0.wp.com/makeoverpakistan.com/wp-content/uploads/2022/08/fixing-spray.png?fit=500%2C500&ssl=1',
+  },
+  {
+    brand: 'MAKEOVER',
+    name: 'HIGH PERFECTION SKINCARE FOUNDATION',
+    image: 'https://beautyboxjo.com/cdn/shop/files/1_098a97d6-20d0-4cc3-a178-1e76f156cac5.jpg?v=1769599808&width=1445',
+  },
+  {
+    brand: 'MAKEOVER',
+    name: 'INTENSIVE SKIN SERUM FOUNDATION',
+    image: 'https://cdn.zochil.shop/9c8c8b6c-a7e5-4581-be54-7939004c4c29_t1500.jpg',
+  },
+  {
+    brand: 'MAKEOVER',
+    name: 'PRO SERIES FACE ESSENTIALS PALETTE',
+    image: 'https://beautyboxjo.com/cdn/shop/files/1_f435e014-9f78-4bfd-91b3-917c7c7d2a34.jpg?v=1775737751&width=1445',
+  },
+];
+
+module.exports = { EXTRA_PRODUCT_IMAGES };
