e01908d2052fd279ff00729320391a6a3bbf36dd
Beirut Bites <beirut.bites.official@gmail.com>
2026-09-21 17:53:20 +0300

    Map first 10 men products to generated images

---
+++ database/local-product-images.js
@@ -0,0 +1,17 @@
+// Curated image paths for local Men/Women/Unisex catalog products.
+// Add new batches here as generated product images are committed under public/images/.
+
+const LOCAL_PRODUCT_IMAGES = [
+  { category: 'men', name: 'A* Men Fantasm Mugler', image: '/images/men/a-men-fantasm-mugler.png' },
+  { category: 'men', name: 'Adidas Game Spirit', image: '/images/men/adidas-game-spirit.png' },
+  { category: 'men', name: 'Adidas Sport', image: '/images/men/adidas-sport.png' },
+  { category: 'men', name: 'Ajwad Lattafa', image: '/images/men/ajwad-lattafa.png' },
+  { category: 'men', name: 'Akira', image: '/images/men/akira.png' },
+  { category: 'men', name: 'Al Haybe', image: '/images/men/al-haybe.png' },
+  { category: 'men', name: 'Allure Homme Sport', image: '/images/men/allure-homme-sport.png' },
+  { category: 'men', name: 'Althaïr de Marly', image: '/images/men/althair-de-marly.png' },
+  { category: 'men', name: 'Amarige', image: '/images/men/amarige.png' },
+  { category: 'men', name: 'Angel', image: '/images/men/angel.png' },
+];
+
+module.exports = { LOCAL_PRODUCT_IMAGES };
