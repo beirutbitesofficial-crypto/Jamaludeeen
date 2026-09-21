003529f8e0f21322a099d4c4cdc2ec21ff1975fe
Beirut Bites <beirut.bites.official@gmail.com>
2026-09-21 18:15:46 +0300

    Map next 10 men products to generated images

--- database/local-product-images.js
+++ database/local-product-images.js
@@ -12,6 +12,16 @@ const LOCAL_PRODUCT_IMAGES = [
   { category: 'men', name: 'Althaïr de Marly', image: '/images/men/althair-de-marly.webp' },
   { category: 'men', name: 'Amarige', image: '/images/men/amarige.webp' },
   { category: 'men', name: 'Angel', image: '/images/men/angel.webp' },
+  { category: 'men', name: 'Antonio Banderas', image: '/images/men/antonio-banderas.webp' },
+  { category: 'men', name: 'Antonio Banderas The Golden Secret', image: '/images/men/antonio-banderas-the-golden-secret.webp' },
+  { category: 'men', name: 'Aqua Bvlgari', image: '/images/men/aqua-bvlgari.webp' },
+  { category: 'men', name: 'Acqua di Gio', image: '/images/men/acqua-di-gio.webp' },
+  { category: 'men', name: 'Acqua di Gio Giorgio Armani', image: '/images/men/acqua-di-gio-giorgio-armani.webp' },
+  { category: 'men', name: 'Acqua di Gio Profondo', image: '/images/men/acqua-di-gio-profondo.webp' },
+  { category: 'men', name: 'Acqua di Gio Profumo', image: '/images/men/acqua-di-gio-profumo.webp' },
+  { category: 'men', name: 'Acqua di Parma Blue', image: '/images/men/acqua-di-parma-blue.webp' },
+  { category: 'men', name: 'Aqua Silva', image: '/images/men/aqua-silva.webp' },
+  { category: 'men', name: 'Aramis', image: '/images/men/aramis.webp' },
 ];
 
 module.exports = { LOCAL_PRODUCT_IMAGES };
