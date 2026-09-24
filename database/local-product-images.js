57e72a1b79e0ed50ce989ebb71ceb6a8486ff388
Beirut Bites <beirut.bites.official@gmail.com>
2026-09-25 01:35:44 +0300

    Map next 10 men products to images

--- database/local-product-images.js
+++ database/local-product-images.js
@@ -22,6 +22,16 @@ const LOCAL_PRODUCT_IMAGES = [
   { category: 'men', name: 'Acqua di Parma Blue', image: '/images/men/acqua-di-parma-blue.png' },
   { category: 'men', name: 'Aqua Silva', image: '/images/men/aqua-silva.png' },
   { category: 'men', name: 'Aramis', image: '/images/men/aramis.png' },
+  { category: 'men', name: 'Armani Black Code', image: '/images/men/armani-black-code.png' },
+  { category: 'men', name: 'Armani Code Absolute', image: '/images/men/armani-code-absolute.png' },
+  { category: 'men', name: 'Asad Lattafa', image: '/images/men/asad-lattafa.png' },
+  { category: 'men', name: 'Asad Lattafa Bourbon', image: '/images/men/asad-lattafa-bourbon.png' },
+  { category: 'men', name: 'Azzaro Wanted', image: '/images/men/azzaro-wanted.png' },
+  { category: 'men', name: 'Azzaro Wanted The Most', image: '/images/men/azzaro-wanted-the-most.png' },
+  { category: 'men', name: 'Bad Boy Carolina Herrera', image: '/images/men/bad-boy-carolina-herrera.png' },
+  { category: 'men', name: 'Bad Boy Cobalt Elixir Carolina Herrera', image: '/images/men/bad-boy-cobalt-elixir-carolina-herrera.png' },
+  { category: 'men', name: 'Bleu de Chanel', image: '/images/men/bleu-de-chanel.png' },
+  { category: 'men', name: 'Boss Bottled', image: '/images/men/boss-bottled.png' },
 ];
 
 module.exports = { LOCAL_PRODUCT_IMAGES };
